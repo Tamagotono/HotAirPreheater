13dd8bd59e20393493ae58879838a4c2be2599bb
Tamagotono <tamagotono@gmail.com>
2012-03-09 19:32:44 -0800

    Renamed most of the defines and variables to make them easier to use.

--- HotAirPreheater.ino
+++ HotAirPreheater.ino
@@ -15,7 +15,7 @@ I chose this library because it allows use of 0,1 or 2 interupt pins for maximum
 The MAX6675 library is also taken from adafruit's github
 https://github.com/adafruit/MAX6675-library
 
-The rest are standard Arduino 1.0 librarys.
+The rest are standard Arduino 1.0 libraries.
 -----------------------------------------------------------------------------
 **** HARDWARE ****
 Rev 0.01
@@ -29,36 +29,29 @@ the input for the second TC and Pin 6 is the CS and wired to DP4.
 #include <Wire.h>
 #include <Encoder.h>
 
-// The pin we use to control the relay
-#define RELAYPIN 13
+// The pin we use to control the SSR
+#define SSRPIN 13
 
-// The SPI pins we use for the thermocouple sensor
-#define MAX_CLK 17
-#define MAX_CS 15
-#define MAX_DATA 16
-#define MAX2_CS 4
+// The pin we use to control the fan speed
+#define FANPIN 3
 
-// the Proportional control constant
-#define Kp  10
-// the Integral control constant
-#define Ki  0.5
-// the Derivative control constant 
-#define Kd  100
+// The SPI pins we use for the TC sensors
+#define SPI_CLK 17
+#define SPI_DATA 16
+
+// The SPI Chip Select (CS) pins
+#define CHIP_CS 15  // CS for the chipTC
+#define AIR_CS 4  // CS for the airTC
+
+// PID pins
+#define Kp  10  // the Proportional control constant
+#define Ki  0.5 // the Integral control constant
+#define Kd  100 // the Derivative control constant 
 
 // Windup error prevention, 5% by default
 #define WINDUPPERCENT 0.05  
 
-MAX6675 thermocouple(MAX_CLK, MAX2_CS, MAX_DATA);
-MAX6675 thermocouple2(MAX_CLK, MAX_CS, MAX_DATA);
-
 // Classic 16x2 LCD used
-// LiquidCrystal display with:
-// rs on pin 12
-// rw is pin 11
-// enable on pin 6
-// backlight control on pin 5 (pwm 0=bright, 511=off)
-// d4, d5, d6, d7 on pins 7, 8, 9, 10
-// backlight pwm control is pin 5
 #define D4 7
 #define D5 8
 #define D6 9
@@ -66,28 +59,30 @@ MAX6675 thermocouple2(MAX_CLK, MAX_CS, MAX_DATA);
 #define RW 11
 #define E 6
 #define RS 12
-#define BL 5
+#define BL 5  // Backlight PWM control
 
 LiquidCrystal lcd(RS, RW, E, D4, D5, D6, D7);
 
-Encoder myEnc(19,2);
+Encoder Encoder(19,2);
 
+//Setup the TCs
+MAX6675 airTC(SPI_CLK, AIR_CS, SPI_DATA);   //temp directly out of the heatgun
+MAX6675 chipTC(SPI_CLK, CHIP_CS, SPI_DATA); //temp measured at the chip/board
 
 // volatile means it is going to be messed with inside an interrupt 
 // otherwise the optimization code will ignore the interrupt
-
-volatile long seconds_time = 0;  // this will get incremented once a second
-volatile float temp1;  // in celsius
-volatile float temp2;  // in celsius
+volatile long  seconds_time = 0;      // this will get incremented once a second
+volatile float airTemp;               // in celsius
+volatile float chipTemp;              // in celsius
 volatile float previous_temperature;  // the last reading (1 second ago)
 
-// the current temperature
-int target_temperature;
+int target_temperature; // the target temperature for the air
+int set_temperature;    // the target temperature for the chip/board
 
 // we need this to be a global variable because we add error each second
 float Summation;        // The integral of error since time = 0
 
-int relay_state;       // whether the relay pin is high (on) or low (off)
+int relay_state;        // whether the relay pin is high (on) or low (off)
 
 void setup() {  
   Serial.begin(9600); 
@@ -101,10 +96,10 @@ void setup() {
   Serial.print(" Ki = "); Serial.print(Ki);
   Serial.print(" Kd = "); Serial.println(Kd);
   
-  // the relay pin controls the plate
-  pinMode(RELAYPIN, OUTPUT);
+  // the relay pin controls the heater
+  pinMode(SSRPIN, OUTPUT);
   // ...and turn it off to start!
-  pinMode(RELAYPIN, LOW);
+  pinMode(SSRPIN, LOW);
 
   // Set up 16x2 standard LCD  
   lcd.begin(16,2);
@@ -116,14 +111,13 @@ void setup() {
   lcd.setCursor(0,1);
   // compile date
   lcd.print(__DATE__);
-  
-  // pause for dramatic effect!
+    // pause for dramatic effect!
   delay(750);
   lcd.clear();
 
   // where we want to be
-  target_temperature = 100.0;  // 100 degrees C
-  myEnc.write(target_temperature); //set the encoder default value
+  target_temperature = 100.0;  // 100 degrees C default starting temp
+  Encoder.write(target_temperature); //set the encoder default value
   
   // set the integral to 0
   Summation = 0;
@@ -138,15 +132,15 @@ void setup() {
  
 void loop() { 
   // we moved the LCD code into the interrupt so we don't have to worry about updating the LCD 
-  // or reading from the thermocouple in the main loop
+  // or reading from the airTC in the main loop
 
   float MV; // Manipulated Variable (ie. whether to turn on or off the relay!)
   float Error; // how off we are
   float Slope; // the change per second of the error
  
   
-  Error = target_temperature - temp1;
-  Slope = previous_temperature - temp1;
+  Error = target_temperature - airTemp;
+  Slope = previous_temperature - airTemp;
   // Summation is done in the interrupt
   
   // proportional-derivative controller only
@@ -157,14 +151,14 @@ void loop() {
   
   if (MV >= 1.0) {
     relay_state = HIGH;
-    digitalWrite(RELAYPIN, HIGH);
+    digitalWrite(SSRPIN, HIGH);
   } else {
     relay_state = LOW;
-    digitalWrite(RELAYPIN, LOW);
+    digitalWrite(SSRPIN, LOW);
   }
 
-
-  int newTarget = myEnc.read();
+  //adjust the target temperature when the encoder turns
+  int newTarget = Encoder.read();
 
   if (newTarget != target_temperature) {
     target_temperature = newTarget;
@@ -181,20 +175,20 @@ SIGNAL(TIMER1_COMPA_vect) {
   seconds_time++;
 
   // save the last reading for our slope calculation
-  previous_temperature = temp1;
+  previous_temperature = airTemp;
 
   // we will want to know the temperature in the main loop()
   // instead of constantly reading it, we'll just use this interrupt
-  // to track it and save it once a second to 'temp1'
-  temp1 = thermocouple.readCelsius();
-  temp2 = thermocouple2.readCelsius();
-//  temp1 = thermocouple.readFarenheit();
+  // to track it and save it once a second to 'airTemp'
+  airTemp = airTC.readCelsius();
+  chipTemp = chipTC.readCelsius();
+//  airTemp = airTC.readFarenheit();
   
   // Sum the error over time
-  Summation += target_temperature - temp1;
+  Summation += target_temperature - airTemp;
   
-  if ( (temp1 < (target_temperature * (1.0 - WINDUPPERCENT))) ||
-       (temp1 > (target_temperature * (1.0 + WINDUPPERCENT))) ) {
+  if ( (airTemp < (target_temperature * (1.0 - WINDUPPERCENT))) ||
+       (airTemp > (target_temperature * (1.0 + WINDUPPERCENT))) ) {
         // to avoid windup, we only integrate within 5%
          Summation = 0;
    }
@@ -210,9 +204,9 @@ SIGNAL(TIMER1_COMPA_vect) {
 
   // go to line #1
   lcd.setCursor(0,1);
-  lcd.print(temp1);
+  lcd.print(airTemp);
   lcd.setCursor(8,1);
-  lcd.print(temp2);
+  lcd.print(chipTemp);
 #if ARDUINO >= 100
   lcd.write(0xDF);
 #else
@@ -223,17 +217,17 @@ SIGNAL(TIMER1_COMPA_vect) {
   // print out a log so we can see whats up
   Serial.print(seconds_time);
   Serial.print("\t");
-  Serial.print(temp1);
+  Serial.print(airTemp);
   Serial.print("\t");
   Serial.print(target_temperature);
   Serial.print("\t");
-  Serial.print(target_temperature - temp1); // the Error!
+  Serial.print(target_temperature - airTemp); // the Error!
   Serial.print("\t");
-  Serial.print(previous_temperature - temp1); // the Slope of the Error
+  Serial.print(previous_temperature - airTemp); // the Slope of the Error
   Serial.print("\t");
   Serial.print(Summation); // the Integral of Error
   Serial.print("\t");
-  Serial.print(Kp*(target_temperature - temp1) + Ki*Summation + Kd*(previous_temperature - temp1)); //  controller output
+  Serial.print(Kp*(target_temperature - airTemp) + Ki*Summation + Kd*(previous_temperature - airTemp)); //  controller output
   Serial.print("\t");
   Serial.println(relay_state);
 } 
